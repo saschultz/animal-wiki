fd0a53445dc2e081a3f3653ed7c8f8155465e32d
Grace Stuart and Sara Schultz <sara@>
2017-02-16 10:01:22 -0800

    declar var for radio button selection.

--- js/animal.js
+++ js/animal.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
     $(".animal").hide();
     $("#button").submit(function(){
-
+      var spots = $("input:radio[#spots]:checked").val();
+      var stripes = $("input:radio[#stripes]:checked").val();
+      var scales =$("input:radio[#scales]:checked").val();
+      
+      if ()
       event.preventDefault();
     });
 });
