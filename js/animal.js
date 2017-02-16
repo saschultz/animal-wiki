0edca081d5483b3f2bded7597b79f09812880de2
Grace Stuart and Sara Schultz <grace@>
2017-02-16 10:39:36 -0800

    correct spelling error in selection and removed the perameter to the last else statement.

--- js/animal.js
+++ js/animal.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
-    $(".animal").hide();
-    $("#button").submit(function(){
-      var spots = $("input:radio[#spots]:checked").val();
-      var stripes = $("input:radio[#stripes]:checked").val();
-      var scales =$("input:radio[#scales]:checked").val();
-      
-      if ()
-      event.preventDefault();
+
+
+   $("#button").click(function(){
+     var selection = $("input:radio[name=looks]:checked").val();
+    //  var stripes = $("input:radio[#stripes]:checked").val();
+    //  var scales =$("input:radio[#scales]:checked").val();
+      if (selection === "spots") {
+        $('.giraffe').show();
+      } else if (selection === "stripes") {
+        $('.zebra').show();
+      } else {
+        $('.armadillo').show();
+      }
+  event.preventDefault();
     });
 });
