e58423db690eb8364f7fe241f78b5bc4105a3d8e
Grace Stuart and Sara Schultz <sara@>
2017-02-16 10:44:29 -0800

    add hide function for other animals in if statements

--- js/animal.js
+++ js/animal.js
@@ -6,10 +6,16 @@ $(document).ready(function() {
     //  var stripes = $("input:radio[#stripes]:checked").val();
     //  var scales =$("input:radio[#scales]:checked").val();
       if (selection === "spots") {
+        $('.zebra').hide();
+        $('.armadillo').hide();
         $('.giraffe').show();
       } else if (selection === "stripes") {
+        $('.giraffe').hide();
+        $('.armadillo').hide();
         $('.zebra').show();
       } else {
+        $('.giraffe').hide();
+        $('.zebra').hide();
         $('.armadillo').show();
       }
   event.preventDefault();
